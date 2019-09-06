42956ab15e45800d84ec7adee03bc5dc77256328
kaixeeee <46895353+kaixeeee@users.noreply.github.com>
2019-09-06 09:21:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,130 +1,44 @@
-﻿const Discord = require("discord.js");
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const client2 = new Discord.Client();
-const myid = [''];
-const prefix = ['-'];
-
-client.on('ready', () => {
-   console.log(`----------------`);
-   console.log(`Credit Farmm - Script By : Kahrbaa `);
-   console.log(`----------------`);
-   console.log(`Loadinng`);
-   console.log(`Loadinng.`);
-   console.log(`Loadinng..`);
-   console.log(`Loadinng...`);
-   console.log(`This Bots Online ' `);
-   console.log(`----------------`);
-});
-
-client2.on('ready', () => {
-   console.log(`----------------`);
-   console.log(`2 Account Online ✩' `);
-   console.log(`----------------`);
-});
 
 
 
-client.on('message', message => {
-    if(message.content === prefix+'راتب'){
-        message.channel.send('#daily')
-    }
-});
+client.on("ready", () => {
+console.log('By : m7md');
+client.user.setPresence({
+  status: 'dnd',
+  game: { 
+     type: 1,
+     name: 'قصتي',
+     details: `غلطة بحق اعز ناس عندي  `,
+     url: 'http://twitch.tv/iizervxyt',
+     state: ` ونفسي يسامحوني بس الفرص راحت `,
+    application_id: '377479790195769345',
+     assets: {
+        small_image: `377480550207717376`,
+        small_text: 'My Best Friends',
+        large_image: `377480353259978752`,
+        large_text: ` Im Sorry akatzuki5 & Joker ` }
 
-client.on('message', message => {
-    if(message.content === prefix+'مبلغ'){
-        message.channel.send('#credits')
-    }
-});
+    
+    
+    
+    
+    
+ 
 
-client.on('message', message => {
-    if(message.content === prefix+'ريب'){
-        message.channel.send("#rep "+"<@" + myid + ">")
-    }
-});
-
-client.on('message', message => {
-if (message.content === prefix+'spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-
-
-if (command == "تحدث") {
-let rank = message.guild.member(message.author).roles.find('name', 'Role.Kahrbaa');
-if (!rank) return message.reply(' ')
-  message.channel.send(args.join("  "))
-    message.delete();
+    
+    
+    
+    
+    
+    
   }
+    });
 });
 
-client2.on('message', message => {
-    if(message.content === prefix+'راتب'){
-        message.channel.send('#daily')
-    }
-});
-
-client2.on('message', message => {
-    if(message.content === prefix+'مبلغ'){
-        message.channel.send('#credits')
-    }
-});
-
-client2.on('message', message => {
-    if(message.content === prefix+'ريب'){
-        message.channel.send('#rep <@286088294234718209>')
-    }
-});
-
-client2.on('message', message => {
-if (message.content === prefix+'spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-client2.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
 
 
-
-if (command == "تحدث") {
-let rank = message.guild.member(message.author).roles.find('name', 'Role.Kahrbaa');
-if (!rank) return message.reply(' ')
-  message.channel.send(args.join("  "))
-    message.delete();
-  }
-});
 
 
 client.login(process.env.TOKEN);
